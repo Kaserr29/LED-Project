7bac36f0682137fc6df7e6239c5a91a1fa7652d8
C_orentin <29.kaserr@gmail.com>
2017-03-07 19:19:40 +0100

    remove -

--- RaspberryPi/views/static/script.js
+++ RaspberryPi/views/static/script.js
@@ -1,14 +1,14 @@
--  function test() {
--    var n = document.getElementById("n").value;
--    var c = document.getElementById("couleur");
--    c.innerHTML = "";
--    for (var i = 0; i < n; i++) {
--      a =i+1;
--      c.innerHTML += '<label for="c'+i+'">Couleur '+a+'</label><input type="color" name="c'+i+'" value="#ff0000" id="c'+i+'">';
--    }
--  }
--
--    function br() {
--      var c = document.getElementById("c0").value;
--      console.log(c);
--    }
+  function test() {
+    var n = document.getElementById("n").value;
+    var c = document.getElementById("couleur");
+    c.innerHTML = "";
+    for (var i = 0; i < n; i++) {
+      a =i+1;
+      c.innerHTML += '<label for="c'+i+'">Couleur '+a+'</label><input type="color" name="c'+i+'" value="#ff0000" id="c'+i+'">';
+    }
+  }
+
+    function br() {
+      var c = document.getElementById("c0").value;
+      console.log(c);
+    }
